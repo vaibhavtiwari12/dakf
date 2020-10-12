2fb42f770703529a69b60dc5176b9c5595c75e19
vaibhav.a.tiwari <vaibhav.a.tiwari@accenture.com>
2020-10-12 11:57:34 +0530

    compressing images and correcting grammer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -97,7 +97,7 @@ jQuery( document ).ready(function( $ ) {
 
         $(".Modern-Slider").slick({
             autoplay:true,
-            autoplaySpeed:5000,
+            autoplaySpeed:50000,
             speed:600,
             slidesToShow:1,
             slidesToScroll:1,
